2caff972782a02f2e4a60c6038d1c9b32827686c
Joy <joyumoekpo1@gmail.com>
2020-12-21 15:05:24 -0600

    add question variables

--- script.js
+++ script.js
@@ -4,4 +4,24 @@ const quiz_questions = [
         choices: ["strings", "booleans", "alerts", "numbers"],
         answer: "strings"
     },
-]
+    {
+        question: "The condition in an if/else statement is enclosed within____",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        question: "String values must be enclosed within __________ when being assigned to variables.",
+        choices: ["commas", "quotes", "stylesheet", "boolean"],
+        answer: "quotes"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is: ____.",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    }
+];
