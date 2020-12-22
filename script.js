5c506e8b7a7ed06a27a78f29e86854b31be21fe9
Joy <joyumoekpo1@gmail.com>
2020-12-21 21:12:45 -0600

    redefined variables and files

--- script.js
+++ script.js
@@ -1,73 +1,93 @@
-var quiz_questions = [
+var cur = 0;
+var correct = 0;
+var quiz, current, question, choices, choiceA, choiceB, choiceC, choiceD;
+var my_questions = [
     {
         question: "What HTML element do we use for JavaScript?",
-        options: {
-            A: "<style>", 
-            B: "<javascript>", 
-            C: "<css>", 
-            D: "<coffeescript>"
-        },
-        answer: "<javascript>"
+        choiceA: "style",
+        choiceB: "javascript",
+        choiceC: "css",
+        choiceD: "coffeescript",
+        answer: "javascript"
     },
     {
         question: "Which is commonly prefered, an internal or external stylesheet?",
-        options: {
-            A: "an internal stylesheet", 
-            B: "an external stylesheet", 
-            C: "neither is preferred", 
-            D: "Why does it matter?"},
+        choiceA: "an internal stylesheet",
+        choiceB: "an external stylesheet",
+        choiceC: "neither is preferred",
+        choiceD: "Why does it matter?",
         answer: "an external stylesheet"
     },
     {
         question: "How would we display an alert?",
-        options: {
-            A:"prompt("")", 
-            B: "confirm("")", 
-            C: "text("")", 
-            D: "alert("")"},
-        answer: "alert("")"
+        choiceA: "prompt('')",
+        choiceB: "confirm('')",
+        choiceC: "text('')",
+        choiceD: "alert('')",
+        answer: "alert('')"
     },
     {
         question: "Should we try to use semantic HTML whenever we can?",
-        options: {
-            A: "Yes", 
-            B: "No", 
-            C: "Not sure", 
-            D: "Why bother?"},
+        choiceA: "Yes",
+        choiceB: "No",
+        choiceC: "Not sure",
+        choiceD: "Why bother?",
         answer: "Yes"
     },
-    {        
+    {
         question: "What should you do when you are frustrated with your code?",
-        choices: {
-            A: "Smash your computer", 
-            B: "Take a deep breath or take a break", 
-            C: "Give up", 
-            D: "Ask your friend to finish your code for you"},
-        answer: "Take a deep breath or take a break"         
+        choiceA: "Smash your computer",
+        choiceB: "Take a deep breath or take a break",
+        choiceC: "Give up",
+        choiceD: "Ask your friend to finish your code for you",
+        answer: "Take a deep breath or take a break"
     }
 ];
 
 
-var startScore = 0;
-var noTime = 0;
-var currentQuestion = 0;
-var timer;
+function get(x) {
+    return document.getElementById(x);
+}
 
-function startQuiz() {    
-    noTime = 100;
-    document.getElementById("starter").innerHTML = noTime;
+function showQuestion() {
+    quiz = get("quiz");
+    if (cur >= my_questions.length) {
+        quiz.innerHTML = "<h2>You got " + correct + " of " + my_questions.length + " questions correct</h2>";
+        get("quiz_status").innerHTML = "Quiz completed";
+        cur = 0;
+        correct = 0;
+        return false;
+    }
 
-    timer = setInterval(function() {
-        noTime--;
-        document.getElementById("starter").innerHTML = noTime;
-        if (noTime <= 0) {
-            clearInterval(timer);
-            quizOver();
+    get("quiz_status").innerHTML = "Question " + (cur + 1) + " of " + my_questions.length;
+
+    question = my_questions[cur].question;
+    choiceA = my_questions[cur].choiceA;
+    choiceB = my_questions[cur].choiceB;
+    choiceC = my_questions[cur].choiceC;
+    choiceD = my_questions[cur].choiceD;
+
+    quiz.innerHTML = "<h3>" + question + "</h3>";
+
+    quiz.innerHTML += "<label> <input type='radio' name='choices' value='A'> " + choiceA + "</label><br>";
+    quiz.innerHTML += "<label> <input type='radio' name='choices' value='B'> " + choiceB + "</label><br>";
+    quiz.innerHTML += "<label> <input type='radio' name='choices' value='C'> " + choiceC + "</label><br>";
+    quiz.innerHTML += "<label> <input type='radio' name='choices' value='D'> " + choiceD + "</label><br><br>";
+    quiz.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button><br>";
+}
+
+function checkAnswer() {
+    choices = document.getElementsByName("choices");
+    for (var i = 0; i < choices.length; i++) {
+        if (choices[i].checked) {
+            choices = choices[i].value;
         }
-    }, 1000);
-    showQuestion();
-};
+    }
 
-var cur = 0;
-var correct = 0;
-var quiz, current, question, choices, choiceA, choiceB, choiceC, choiceD;
+    if (choices == my_questions[cur].answer) {
+        correct++;
+    }
+    cur++;
+    showQuestion();
+}
+window.addEventListener("load", showQuestion);
